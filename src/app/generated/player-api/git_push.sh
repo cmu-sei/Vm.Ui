f3a64acb50dca8a3ea47713a88c5f5e5053964b9
sei-byoung <72205768+sei-byoung@users.noreply.github.com>
2021-01-26 13:27:14 -0500

    Update generated code (#69)

--- src/app/generated/player-api/git_push.sh
+++ src/app/generated/player-api/git_push.sh
@@ -1,3 +1,6 @@
+# Copyright 2021 Carnegie Mellon University. All Rights Reserved.
+# Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
+
 #!/bin/sh
 # ref: https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
 #
