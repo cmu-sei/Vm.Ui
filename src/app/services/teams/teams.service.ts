9a721bdf305d2d53d1c96ce21812cb813f179d1e
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2021-02-09 14:29:54 -0500

    fixes iso upload url (#70)
    - use player api BASE_PATH injection token

--- src/app/services/teams/teams.service.ts
+++ src/app/services/teams/teams.service.ts
@@ -2,20 +2,21 @@
 // Released under a MIT (SEI)-style license. See LICENSE.md in the project root for license information.
 
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { ComnSettingsService } from '@cmusei/crucible-common';
+import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { BASE_PATH } from '../../generated/player-api';
 import { TeamData } from '../../models/team-data';
 
 @Injectable()
 export class TeamsService {
-  constructor(
-    private http: HttpClient,
-    private settings: ComnSettingsService
-  ) {}
+  playerApiUrl: string;
+
+  constructor(private http: HttpClient, @Inject(BASE_PATH) basePath: string) {
+    this.playerApiUrl = basePath;
+  }
 
   public GetAllMyTeams(viewId: string): Observable<Array<TeamData>> {
-    const url = `${this.settings.settings.ApiPlayerUrl}/api/me/views/${viewId}/teams`;
+    const url = `${this.playerApiUrl}/api/me/views/${viewId}/teams`;
     return this.http.get<Array<TeamData>>(url);
   }
 }
